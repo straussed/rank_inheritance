e15fc65e58f693b45fd121ef271d72a6d8b8bf74
Eli Strauss <straussed@gmail.com>
2018-08-20 18:06:08 -0400

    Updated instructions

--- hz_rank_inheritance.R
+++ hz_rank_inheritance.R
@@ -11,8 +11,8 @@
 ################################################################################
 ## Here is a script for making animated plots of rank acquisition
 ## from interaction data. To use the script right out of the box: 
-##    1) download all all files in the github repo to a single folder
-##    2) open the .Rproj file
+##    1) download all files in the github repo to a single folder
+##    2) open the .Rproj file in Rstudio
 ##    3) in Rstudio, open the script
 ##    4) make sure all required packages and ImageMagick are installed
 ##    5) run the script
